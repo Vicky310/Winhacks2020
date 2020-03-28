64895b919888ca44ef96c8d9ecaa2a9fc4870725
Vigneshwaran <vigneshwaran0310@gmail.com>
2020-03-28 18:21:57 -0400

    Post UI changes

--- app/src/Components/AddPost/AddPost.js
+++ app/src/Components/AddPost/AddPost.js
@@ -1,39 +1,59 @@
 import React, { Component } from 'react';
+import {Input,Button} from 'antd';
 
-import './AddPost.css';
+import classes from  './AddPost.module.css';
+
 
 class AddPost extends Component {
     state = {
         title: '',
-        content: ''
+        content: '',
+        disabled: true
     }
 
+    // componentDidMount() {
+    //     this.setState( {
+    //         disabled: !this.state.disabled
+    //     })
+    // }
+
     contentChangedHandler = (event) => {
-        this.setState({content: event.target.value});
+        this.setState({content: event.target.value, disabled: !(event.target.value && this.state.title)});
     }
 
     titleChangedHandler = (event) => {
-        this.setState({title: event.target.value});
+        this.setState({title: event.target.value, disabled: !(event.target.value && this.state.content)});
     }
 
-    // onPost = () => {
-    //     this.props.postAdded(this.state.title, this.state.content);
-    // }
+    onPost = () => {
+        this.props.postAdded(this.state.title, this.state.content);
+        this.setState({
+            title: '',
+            content: '',
+            disabled: true
+        });
+    }
 
     render () {
+        const { TextArea } = Input;
         return (
+            
             <div className="AddPost">
-                <input 
+                <Input 
+                    className={classes.inp}
                     type="text" 
                     placeholder="Title" 
                     onChange={this.titleChangedHandler}
-                    value={this.state.title} />
-                <input 
+                    value={this.state.title} required/>
+                    <br/>
+                <TextArea
+                className={classes.inp}
                     type="text" 
                     placeholder="Content" 
                     onChange={this.contentChangedHandler}
-                    value={this.state.content} />
-                <button onClick={() => this.props.postAdded(this.state.title, this.state.content)}>Post</button>
+                    value={this.state.content} required />
+                    <br/>
+                <Button disabled={this.state.disabled} className="primary" onClick={this.onPost}>Post</Button>
             </div>
         );
     }
