710827ca69b6bb991cc3db46563427af6e0280c2
Yash Sharma <mr.sharmayash@outlook.com>
2020-03-28 16:11:30 -0700

    added method to pull posts from database

--- app/test/htmlGeoLocation.js
+++ app/test/htmlGeoLocation.js
@@ -1,14 +1,31 @@
-let url = "https://winhacks2020-88149.firebaseio.com/Users.json"
-var users = axios.get(url).then(resource => {
-  //  console.log(resource.data);
-     
-     const keys = Object.keys(resource.data)
-     const values = Object.values(resource.data)
-     console.log(keys)
-     for(val of values){
-         const subkeys = Object.keys(val)
-         console.log(val.latitude)
-         console.log(val.longitude)
-     }
-})
 
+
+
+userId = localStorage.getItem('userId')
+var usercommunity;
+let url = "https://winhacks2020-88149.firebaseio.com/Users.json";
+var users = axios.get(url).then(resource => {
+  //  console.log(resource.data)
+     const keys = Object.entries(resource.data)
+     const values = Object.values(resource.data)
+    
+     for(key of keys){
+        if(key[0] == userId){
+            usercommunity = key[1].community;
+            let url2 = "https://winhacks2020-88149.firebaseio.com/community/" + usercommunity + ".json";
+            var community = axios.get(url2).then(resource => {
+              //  console.log(resource.data)
+                 const keys = Object.entries(resource.data)
+                 const values = Object.values(resource.data)
+                
+                 for(key of keys){
+
+                    //this is the line that logs out the posts in a community
+                    console.log(key[1].postContent)
+                }
+                 
+            });
+        }
+    }
+     
+});
