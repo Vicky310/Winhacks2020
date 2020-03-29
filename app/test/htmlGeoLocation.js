1fcb690d1defb1fb29e5d4570f911827f937239a
Yash Sharma <mr.sharmayash@outlook.com>
2020-03-28 18:21:41 -0700

    test code for assigning communities to users

--- app/test/htmlGeoLocation.js
+++ app/test/htmlGeoLocation.js
@@ -2,30 +2,12 @@
 
 
 userId = localStorage.getItem('userId')
+var data = {
+    community: "default3"
+}
+userId = 'yXxdJ3l0rnWyQydcQ4FC4ZkpIY53'
 var usercommunity;
-let url = "https://winhacks2020-88149.firebaseio.com/Users.json";
-var users = axios.get(url).then(resource => {
-  //  console.log(resource.data)
-     const keys = Object.entries(resource.data)
-     const values = Object.values(resource.data)
-    
-     for(key of keys){
-        if(key[0] == userId){
-            usercommunity = key[1].community;
-            let url2 = "https://winhacks2020-88149.firebaseio.com/community/" + usercommunity + ".json";
-            var community = axios.get(url2).then(resource => {
-              //  console.log(resource.data)
-                 const keys = Object.entries(resource.data)
-                 const values = Object.values(resource.data)
-                
-                 for(key of keys){
-
-                    //this is the line that logs out the posts in a community
-                    console.log(key[1].postContent)
-                }
-                 
-            });
-        }
-    }
-     
+let url = "https://winhacks2020-88149.firebaseio.com/Users/" + userId + ".json";
+var users = axios.patch(url, data).then(resource => {
+    console.log(resource)
 });
