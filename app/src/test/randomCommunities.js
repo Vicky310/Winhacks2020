c985c658e9b547073e29bc90bd77226d0bd4459a
Yash Sharma <mr.sharmayash@outlook.com>
2020-03-29 06:07:59 -0700

    fixed jumping algo

--- app/src/test/randomCommunities.js
+++ app/src/test/randomCommunities.js
@@ -8,15 +8,18 @@ export default function getCommunities(){
       for (let key in res.data) {
         array.push(keY);
       }
-    });
 
     if(array.length <= 6) return array;
 
     var choosen = [];
     for(let i = 0; i < 6; i++){
         let num = Math.floor(Math.random() * array.length); // generate random num 0 - (array.length-1)
+        choosen[i] = array[num];
         array.splice(num, 1); // remove index num
     }
     return choosen;
+    });
+
+    
     
 }
